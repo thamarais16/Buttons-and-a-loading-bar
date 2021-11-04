1ca3c7c2c8f8b8fc49e47f8c52aad8d786f93afc
thamarais16 <thamarais16@gmail.com>
2021-11-04 22:56:02 +0530

    3 files modified

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log('hello!')
+/**
+ * @description function used to add/remove classlist for buttons and progress bar.
+ * 
+ */
+function startProgress(){
+  document.getElementById("progressBar").classList.add("visible-progress-bar");
+  document.getElementById("uploadButton").classList.add("disable-button");
+  document.getElementById("downloadButton").classList.add("disable-button");
+
+  setTimeout(()=>{
+    document.getElementById("progressBar").classList.remove("visible-progress-bar");
+    document.getElementById("uploadButton").classList.remove("disable-button");
+    document.getElementById("downloadButton").classList.remove("disable-button");
+  }, 4 * 1000);
+}
